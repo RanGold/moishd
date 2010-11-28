be26a81746c15c09a28bea0c12195cde4a36be5f
barkamir@gmail.com <barkamir@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2010-11-28 18:31:51 +0000

    added RegistrationServlet.java for handling c2dm registraion per user. currently just sending DevRegAndIDPairs object for testing, but need to be replaced with the proper User class.
    Ran - once the class will be complete, you can just import it from the HttpRequest and add reg_id to the user by the google account.
    i added the code for sending push to the RegistrationServlet.java, justs for testing. once there will be servlets that are responsible for sending diffrent types of push - this code will be copied to relevant places.

---
+++ Trunk/ServerTrunk/src/moishd/server/DevRegAndIDPairs.java
@@ -0,0 +1,35 @@
+package moishd.server;
+//this class is just for testing until Ran will create the suitable UserClass
+//this class will be deleted.
+
+import java.io.Serializable;
+
+public class DevRegAndIDPairs implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String deviceReg;
+	
+	private String registration_ID;
+	
+	public DevRegAndIDPairs(String deviceReg, String reg_id){
+		this.deviceReg = deviceReg;
+		this.registration_ID = reg_id;
+	}
+	
+	public String getDevReg(){
+		return deviceReg;
+	}
+	
+	public String getRegId(){
+		return registration_ID;
+	}	
+	
+	public DevRegAndIDPairs()
+	{
+		this.deviceReg = null;
+		this.registration_ID = null;
+	}
+
+	
+}
