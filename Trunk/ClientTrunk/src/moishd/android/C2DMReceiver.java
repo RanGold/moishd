60186151e36cb3d560753c1a391aa4bf3c3c0d0e
hila.barzilai <hila.barzilai@d2c90892-111c-e7c7-7384-095ed294f940>
2010-12-02 22:16:22 +0000

    changed activity flag in onMessage method

--- Trunk/ClientTrunk/src/moishd/android/C2DMReceiver.java
+++ Trunk/ClientTrunk/src/moishd/android/C2DMReceiver.java
@@ -40,7 +40,7 @@ public class C2DMReceiver extends C2DMBaseReceiver {
 
 		Intent usersTabIntent = new Intent();
 		usersTabIntent.setClass(getApplicationContext(), AllOnlineUsersActivity.class);
-		usersTabIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		usersTabIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		usersTabIntent.putExtra("push_game_id", game_id);
 
 		if (action.equals("GameInvitation")){
