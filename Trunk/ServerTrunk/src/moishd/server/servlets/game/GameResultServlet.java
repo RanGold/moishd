600aaab08c266236d7f1020c727febc94adcbcc7
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2010-12-22 20:17:18 +0000

    Added queues use

--- Trunk/ServerTrunk/src/moishd/server/servlets/game/GameResultServlet.java
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/GameResultServlet.java
@@ -2,19 +2,20 @@ package moishd.server.servlets.game;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import moishd.server.common.C2DMCommon;
 import moishd.server.common.DSCommon;
 import moishd.server.common.DataAccessException;
 import moishd.server.common.LoggerCommon;
 import moishd.server.dataObjects.MoishdGame;
-import moishd.server.dataObjects.MoishdUser;
 import moishd.server.servlets.GeneralServlet;
+
+import com.google.appengine.api.labs.taskqueue.Queue;
+import com.google.appengine.api.labs.taskqueue.QueueFactory;
+import com.google.appengine.api.labs.taskqueue.TaskOptions;
+import com.google.appengine.api.labs.taskqueue.TaskOptions.Method;
 
 public class GameResultServlet extends GeneralServlet {
 	/**
@@ -50,57 +51,18 @@ public class GameResultServlet extends GeneralServlet {
 				} else if (tg.getPlayerRecId().equals(user.getEmail())) {
 					tg.setPlayerRecEndTime(endDate);
 				} else {
-					response.addHeader("Error", "");
-					response.getWriter().println(servletName + ": player " + 
+					LoggerCommon.Get().LogError(this, response, servletName + ": player " + 
 							user.getEmail() + " isn't in game " + gameId);
 					return;
 				}
 				tg.SaveChanges();
 				
-				tg = DSCommon.GetGameById(gameId);
+				Queue queue = QueueFactory.getQueue("resultQueue");
+				queue.add (TaskOptions.Builder.url("/queues/SendGameResult").method(Method.POST).
+						param("gameId", gameId).param("gameType", gameType).
+						param("winValue", winValue.toString()).param("loseValue", loseValue.toString()));
 				
-				if (!tg.getIsDecided()) {
-					tg.setIsDecided(true);
-					tg.SaveChanges();
-					
-					MoishdUser mInitUser = DSCommon.GetUserByGoogleId(tg.getPlayerInitId());
-					MoishdUser mRecUser = DSCommon.GetUserByGoogleId(tg.getPlayerRecId());
-					
-					mInitUser.setBusy(false);
-					mInitUser.SaveChanges();
-					mRecUser.setBusy(false);
-					mRecUser.SaveChanges();
-					
-					HashMap<String, String> winPayload = new HashMap<String, String>();
-					winPayload.put("GameId", String.valueOf(tg.getGameId().getId()));
-					winPayload.put("Result", winValue.toString() + ":" + gameType);
-					
-					HashMap<String, String> losePayload = new HashMap<String, String>();
-					losePayload.put("GameId", String.valueOf(tg.getGameId().getId()));
-					losePayload.put("Result", loseValue.toString() + ":" + gameType);
-					
-					tg = DSCommon.GetGameById(gameId);
-					
-					// TODO: Check if concurrent win is consistent
-					if ((tg.getPlayerRecEndTime() == null) ||
-							((tg.getPlayerInitEndTime() != null) && 
-									(tg.getPlayerRecEndTime().after(tg.getPlayerInitEndTime())))) {
-						if (user.getEmail().equals(mInitUser.getUserGoogleIdentifier())) {
-							C2DMCommon.PushGenericMessage(mInitUser.getRegisterID(), 
-									C2DMCommon.Actions.GameResult.toString(), winPayload);
-							C2DMCommon.PushGenericMessage(mRecUser.getRegisterID(), 
-									C2DMCommon.Actions.GameResult.toString(), losePayload);
-						}
-					} else if (user.getEmail().equals(mRecUser.getUserGoogleIdentifier())) {
-						C2DMCommon.PushGenericMessage(mRecUser.getRegisterID(), 
-								C2DMCommon.Actions.GameResult.toString(), winPayload);
-						C2DMCommon.PushGenericMessage(mInitUser.getRegisterID(), 
-								C2DMCommon.Actions.GameResult.toString(), losePayload);
-					}
-				}
 			} catch (DataAccessException e) {
-				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
-			} catch (ServletException e) {
 				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
 			}
 		}
