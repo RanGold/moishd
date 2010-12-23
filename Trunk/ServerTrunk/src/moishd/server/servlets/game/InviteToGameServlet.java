93bc8c051eebac05896c595420156ff8e8254b7a
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2010-12-23 15:56:33 +0000

    Added Queues use in game invitation

--- Trunk/ServerTrunk/src/moishd/server/servlets/game/InviteToGameServlet.java
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/InviteToGameServlet.java
@@ -1,23 +1,16 @@
 package moishd.server.servlets.game;
 
 import java.io.IOException;
-import java.util.HashMap;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import moishd.client.dataObjects.ClientMoishdUser;
-import moishd.server.common.C2DMCommon;
-import moishd.server.common.DSCommon;
-import moishd.server.common.DataAccessException;
-import moishd.server.common.GsonCommon;
-import moishd.server.common.LoggerCommon;
-import moishd.server.dataObjects.MoishdGame;
-import moishd.server.dataObjects.MoishdUser;
 import moishd.server.servlets.GeneralServlet;
 
-import com.google.gson.reflect.TypeToken;
+import com.google.appengine.api.labs.taskqueue.Queue;
+import com.google.appengine.api.labs.taskqueue.QueueFactory;
+import com.google.appengine.api.labs.taskqueue.TaskOptions;
+import com.google.appengine.api.labs.taskqueue.TaskOptions.Method;
 
 public class InviteToGameServlet extends GeneralServlet {
 	/**
@@ -31,39 +24,11 @@ public class InviteToGameServlet extends GeneralServlet {
 		super.doPost(request, response);
 
 		if (user != null) {
-			try {
-				MoishdUser initUser = DSCommon.GetUserByGoogleId(user.getEmail());
-				
-				ClientMoishdUser clientUser = 
-					GsonCommon.GetObjFromJsonStream(request.getInputStream(), 
-							new TypeToken<ClientMoishdUser>(){}.getType());
-				MoishdUser invUser = DSCommon.GetUserByGoogleId(clientUser.getUserGoogleIdentifier());
-				
-				if (invUser.isBusy()) {
-					C2DMCommon.PushGenericMessage(initUser.getRegisterID(), 
-							C2DMCommon.Actions.PlayerBusy.toString(), new HashMap<String, String>());
-				} else {
-					initUser.setBusy(true);
-					initUser.SaveChanges();
-					invUser.setBusy(true);
-					invUser.SaveChanges();
-					
-					MoishdGame tg = new MoishdGame(user.getEmail(), clientUser.getUserGoogleIdentifier());
-					tg.SaveChanges();
-					response.getWriter().write(String.valueOf(tg.getGameLongId()));
-
-					HashMap<String, String> payload = new HashMap<String, String>();
-					payload.put("GameId", String.valueOf(tg.getGameId().getId()));
-					C2DMCommon.PushGenericMessage(invUser.getRegisterID(), 
-							C2DMCommon.Actions.GameInvitation.toString(), payload);
-				}
-			} catch (DataAccessException e) {
-				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
-			} catch (ClassNotFoundException e) {
-				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
-			} catch (ServletException e) {
-				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
-			}
+			String recID = request.getReader().readLine();
+			
+			Queue queue = QueueFactory.getQueue("inviteQueue");
+			queue.add (TaskOptions.Builder.url("/queues/SendInviteToGame").method(Method.POST).
+					param("initID", user.getEmail()).param("recID", recID));
 		}
 	}
 }
