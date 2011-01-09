83ea6edb7a7e8119f58b73a5a10277d27990b360
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2011-01-09 00:53:03 +0000

    is busy servlet

---
+++ Trunk/ServerTrunk/src/moishd/server/servlets/IsBusyServlet.java
@@ -0,0 +1,25 @@
+package moishd.server.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IsBusyServlet extends GeneralServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8401273278133734085L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		checkBusy = false;
+		super.doPost(request, response);
+
+		if (user != null) {
+			if (mUser.isBusy()) {
+				response.addHeader("HasLocation", "");
+			}
+		}
+	}
+}
