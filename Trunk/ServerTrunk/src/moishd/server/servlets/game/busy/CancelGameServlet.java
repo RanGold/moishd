d42d11cf153247a17e2941dc95eb82578a73ad5c
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2011-01-10 09:44:37 +0000

    file structural difference only

---
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/busy/CancelGameServlet.java
@@ -0,0 +1,36 @@
+package moishd.server.servlets.game.busy;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import moishd.server.common.DSCommon;
+import moishd.server.common.DataAccessException;
+import moishd.server.common.LoggerCommon;
+import moishd.server.dataObjects.MoishdUser;
+import moishd.server.servlets.GeneralServlet;
+
+public class CancelGameServlet extends GeneralServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8947016425378186670L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		super.doPost(request, response);
+
+		if (user != null) {
+			try {
+				MoishdUser otherUser = DSCommon.GetUserByGoogleId(mUser.getBusyWith());
+				mUser.setNotBusy();
+				mUser.SaveChanges();
+				otherUser.setNotBusy();
+				otherUser.SaveChanges();
+			} catch (DataAccessException e) {
+				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
+			}
+		}
+	}
+}
--- Trunk/ServerTrunk/src/moishd/server/servlets/game/busy/SetNotBusyServlet.java
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/busy/SetNotBusyServlet.java
@@ -1,4 +1,4 @@
-package moishd.server.servlets;
+package moishd.server.servlets.game.busy;
 
 import java.io.IOException;
 
@@ -6,12 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import moishd.server.common.LoggerCommon;
+import moishd.server.servlets.GeneralServlet;
 
-public class SetBusyServlet extends GeneralServlet {
+public class SetNotBusyServlet extends GeneralServlet {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8141924964503496779L;
+	private static final long serialVersionUID = 8482003842059400287L;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
@@ -19,11 +20,11 @@ public class SetBusyServlet extends GeneralServlet {
 		super.doPost(request, response);
 
 		if (user != null) {
-			if (mUser.isBusy()) {
+			if (!mUser.isPartnerWith(mUser.getUserGoogleIdentifier())) {
 				LoggerCommon.Get().LogError(this, response,
-					"Tried to get busy (with himself) while busy with " + mUser.getBusyWith());
+					"Tried to get un busy (with himself) while busy with " + mUser.getBusyWith());
 			} else {
-				mUser.setPartner(mUser.getUserGoogleIdentifier());
+				mUser.setNotBusy();
 				mUser.SaveChanges();
 			}
 		}
