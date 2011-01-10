9eb02fa86b738d45ffc97b43e3b953eb7e04824c
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2011-01-10 20:03:17 +0000

    Most popular game changed

--- Trunk/ServerTrunk/src/moishd/server/servlets/game/GetMostPopularGameServlet.java
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/GetMostPopularGameServlet.java
@@ -1,14 +1,12 @@
 package moishd.server.servlets.game;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import moishd.server.common.DSCommon;
 import moishd.server.common.LoggerCommon;
-import moishd.server.dataObjects.GameStatistics;
 import moishd.server.servlets.GeneralServlet;
 
 public class GetMostPopularGameServlet extends GeneralServlet{
@@ -24,15 +22,9 @@ public class GetMostPopularGameServlet extends GeneralServlet{
 		super.doPost(request, response);
 
 		if (user != null) {
-			List<GameStatistics> stats = DSCommon.GetMostPopularGame();
-			String gameType;
-			if (stats.size() == 0) {
-				gameType = "DareSimonPro";
-			} else {
-				gameType = stats.get(0).getGameType();
-			}
+			String gameType = DSCommon.GetMostPopularGame();
 			
-			LoggerCommon.Get().LogInfo(this, gameType);
+			LoggerCommon.Get().LogInfo(this, "Popular game returned " + gameType);
 			
 			response.getWriter().print(gameType);
 		}
