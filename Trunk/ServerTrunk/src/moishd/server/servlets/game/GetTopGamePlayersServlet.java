591e6aa1b6dcd65ddcf45dc5fb893d47edbd3843
mran.goldberg@gmail.com <mran.goldberg@gmail.com@d2c90892-111c-e7c7-7384-095ed294f940>
2011-01-13 20:31:02 +0000

    GetTopGmaePlayers

---
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/GetTopGamePlayersServlet.java
@@ -0,0 +1,47 @@
+package moishd.server.servlets.game;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import moishd.server.common.DSCommon;
+import moishd.server.common.DataAccessException;
+import moishd.server.common.GsonCommon;
+import moishd.server.common.LoggerCommon;
+import moishd.server.dataObjects.GameStatistics;
+import moishd.server.dataObjects.MoishdUser;
+import moishd.server.dataObjects.StringIntPair;
+import moishd.server.servlets.GeneralServlet;
+
+public class GetTopGamePlayersServlet extends GeneralServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5452032628927024429L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException {
+
+		super.doPost(request, response);
+
+		if (user != null) {
+			String gameName = request.getReader().readLine();
+			GameStatistics stat = DSCommon.GetGameStatByName(gameName);
+			
+			List<MoishdUser> users = new LinkedList<MoishdUser>();
+			for (StringIntPair siPair : stat.getTopMoishers()) {
+				try {
+					users.add(DSCommon.GetUserByGoogleId(siPair.getStringValue()));
+				} catch (DataAccessException e) {
+					LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
+				}
+			}
+			
+			GsonCommon.WriteJsonToResponse(users, response);
+		}
+	}
+
+}
