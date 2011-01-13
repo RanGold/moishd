0db6251e20091f77b9f9210a01af153f32d33601
hila.barzilai <hila.barzilai@d2c90892-111c-e7c7-7384-095ed294f940>
2011-01-13 16:37:40 +0000

    added Map<String, Integer> to GameStatistics in order to keep score of points by game for each user. added the required logics in SendGameResultServlet

--- Trunk/ServerTrunk/src/moishd/server/servlets/game/busy/SendGameResultServlet.java
+++ Trunk/ServerTrunk/src/moishd/server/servlets/game/busy/SendGameResultServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,9 +31,9 @@ public class SendGameResultServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = -530008367358724317L;
-	
+
 	private int factorNearBy = 1;
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 	throws IOException {
 		if (request.getHeader("X-AppEngine-QueueName").equals("resultQueue")) {
@@ -41,24 +42,24 @@ public class SendGameResultServlet extends HttpServlet {
 				String gameType = request.getParameter("gameType");
 				String winValue = request.getParameter("winValue");
 				String loseValue = request.getParameter("loseValue");
-				
+
 				MoishdGame tg = DSCommon.GetGameById(gameId);
-				
+
 				if (!tg.getIsDecided()) {
 					tg.setIsDecided(true);
 					tg.SaveChanges();
-					
+
 					MoishdUser mInitUser = DSCommon.GetUserByGoogleId(tg.getPlayerInitId());
 					MoishdUser mRecUser = DSCommon.GetUserByGoogleId(tg.getPlayerRecId());
-					
+
 					MoishdUser winner;
 					MoishdUser loser;
-					
+
 					tg = DSCommon.GetGameById(gameId);
 					if ((tg.getPlayerRecEndTime() == null) ||
-						((tg.getPlayerInitEndTime() != null) && 
-						 (tg.getPlayerRecEndTime().after(tg.getPlayerInitEndTime())))) {
-						
+							((tg.getPlayerInitEndTime() != null) && 
+									(tg.getPlayerRecEndTime().after(tg.getPlayerInitEndTime())))) {
+
 						winner = mInitUser;
 						loser = mRecUser;
 					}
@@ -67,33 +68,30 @@ public class SendGameResultServlet extends HttpServlet {
 						loser = mInitUser;
 					}
 
-					
 					HashMap<String, String> winPayload = new HashMap<String, String>();
 					winPayload.put("GameId", String.valueOf(tg.getGameId().getId()));
 					winPayload.put("Result", winValue.toString() + ":" + gameType);
-			
+
 					HashMap<String, String> losePayload = new HashMap<String, String>();
 					losePayload.put("GameId", String.valueOf(tg.getGameId().getId()));
 					losePayload.put("Result", loseValue.toString() + ":" + gameType);
 
-					
 					if (winner.getLocation().isInitialized() && 
 							loser.getLocation().isInitialized() &&
 							DSCommon.CalculateDistance(winner.getLocation(), loser.getLocation()) <=1){
 						LoggerCommon.Get().LogInfo(this, "winner location is " + winner.getLocation());
 						LoggerCommon.Get().LogInfo(this, "loser location is " + loser.getLocation());
 						LoggerCommon.Get().LogInfo(this, "distance is " + DSCommon.CalculateDistance(winner.getLocation(), loser.getLocation()));
-						
-						
-								factorNearBy = 2;
-								winPayload.put("NearByGame", "yes");
-								losePayload.put("NearByGame", "yes");
-							}
+
+						factorNearBy = 2;
+						winPayload.put("NearByGame", "yes");
+						losePayload.put("NearByGame", "yes");
+					}
 					else {
 						winPayload.put("NearByGame", "no");
 						losePayload.put("NearByGame", "no");
 					}
-					
+
 					if (winValue.toString().equals("Won")){
 						int [] pointsAddedToBothSides = UpdateGameStatistics(tg, winner, loser);
 						winPayload.put("Points", String.valueOf(pointsAddedToBothSides[0]));
@@ -104,57 +102,18 @@ public class SendGameResultServlet extends HttpServlet {
 						winPayload.put("Points", String.valueOf(pointsAddedToBothSides[1]));
 						losePayload.put("Points", String.valueOf(pointsAddedToBothSides[0]));	
 					}
-					
-					int winnerOldPoints = winner.getStats().getPoints();
-					List<TrophiesEnum> winnerOldTrophies = new LinkedList<TrophiesEnum>(winner.getTrophies());
-					int loserOldPoints = winner.getStats().getPoints();
-					List<TrophiesEnum> loserOldTrophies = new LinkedList<TrophiesEnum>(loser.getTrophies());
-					
+
 					updateRankAndTrophies(winPayload, winner);
 					updateRankAndTrophies(losePayload, loser);
-					
-					if (winner.getUserGoogleIdentifier().equals(tg.getPlayerInitId())) {
-						tg.setPlayerInitPoints(winner.getStats().getPoints() - winnerOldPoints);
-						tg.setPlayerRecPoints(loser.getStats().getPoints() - loserOldPoints);
-						
-						for (TrophiesEnum trophy : winner.getTrophies()) {
-							if (!winnerOldTrophies.contains(trophy)) {
-								tg.getPlayerInitTrophies().add(trophy);
-							}
-						}
-						
-						for (TrophiesEnum trophy : loser.getTrophies()) {
-							if (!loserOldTrophies.contains(trophy)) {
-								tg.getPlayerRecTrophies().add(trophy);
-							}
-						}
-					} else {
-						tg.setPlayerRecPoints(winner.getStats().getPoints() - winnerOldPoints);
-						tg.setPlayerInitPoints(loser.getStats().getPoints() - loserOldPoints);
-						
-						for (TrophiesEnum trophy : winner.getTrophies()) {
-							if (!winnerOldTrophies.contains(trophy)) {
-								tg.getPlayerRecTrophies().add(trophy);
-							}
-						}
-						
-						for (TrophiesEnum trophy : loser.getTrophies()) {
-							if (!loserOldTrophies.contains(trophy)) {
-								tg.getPlayerInitTrophies().add(trophy);
-							}
-						}
-					}
-					
-					tg.SaveChanges();
-				
+
 					C2DMCommon.PushGenericMessage(winner.getRegisterID(), 
-												  C2DMCommon.Actions.GameResult.toString(), winPayload);
+							C2DMCommon.Actions.GameResult.toString(), winPayload);
 					C2DMCommon.PushGenericMessage(loser.getRegisterID(), 
-												  C2DMCommon.Actions.GameResult.toString(), losePayload);
-					
+							C2DMCommon.Actions.GameResult.toString(), losePayload);
+
 					List<BusyObject> busyUsers = new LinkedList<BusyObject>();
 					busyUsers
-							.add(new BusyObject(mInitUser.getUserGoogleIdentifier(), false, mRecUser.getBusyWith()));
+					.add(new BusyObject(mInitUser.getUserGoogleIdentifier(), false, mRecUser.getBusyWith()));
 					busyUsers.add(new BusyObject(mRecUser.getBusyWith(), false, mInitUser.getUserGoogleIdentifier()));
 
 					String json = GsonCommon.GetJsonString(busyUsers);
@@ -163,12 +122,12 @@ public class SendGameResultServlet extends HttpServlet {
 					queue.add(TaskOptions.Builder
 							.url("/queues/UpdateBusySynced").method(Method.POST)
 							.param("json", json));
-//					mInitUser.setNotBusy();
-//					mInitUser.SaveChanges();
-//					mRecUser.setNotBusy();
-//					mRecUser.SaveChanges();
+					//					mInitUser.setNotBusy();
+					//					mInitUser.SaveChanges();
+					//					mRecUser.setNotBusy();
+					//					mRecUser.SaveChanges();
 				}
-				
+
 			} catch (DataAccessException e) {
 				LoggerCommon.Get().LogError(this, response, e.getMessage(), e.getStackTrace());
 			} catch (ServletException e) {
@@ -176,30 +135,30 @@ public class SendGameResultServlet extends HttpServlet {
 			}
 		}
 	}
-	
+
 	private int [] UpdateGameStatistics(MoishdGame moishdGame, MoishdUser winner, MoishdUser loser){
-		
-		
+
+
 		//Increment number of games played for both users
 		int winnerGamesPlayed = winner.getStats().getGamesPlayed();
 		winner.getStats().setGamesPlayed(winnerGamesPlayed + 1);
-		
+
 		int loserGamesPlayed= loser.getStats().getGamesPlayed();
 		loser.getStats().setGamesPlayed(loserGamesPlayed + 1);
-		
+
 		//Update winner's won games statistics
 		int winnerGamesWon = winner.getStats().getGamesWon();
 		winner.getStats().setGamesWon(winnerGamesWon+1);
-		
+
 		int winnerGamesWonInARow = winner.getStats().getGamesWonInARow();
 		winner.getStats().setGamesWonInARow(winnerGamesWonInARow+1);
-		
+
 		//Update loser's won games statistics
 		loser.getStats().setGamesWonInARow(0);
-		
+
 		int winnerPoints = winner.getStats().getPoints();
 		int loserPoints = loser.getStats().getPoints();
-		
+
 		int [] addedPoints = new int[2];
 		//Do points logic
 		if (moishdGame.getGameType().equals(C2DMCommon.Actions.StartGameTruth.toString())){
@@ -223,26 +182,34 @@ public class SendGameResultServlet extends HttpServlet {
 			addedPoints[1] = 1*factorNearBy;
 		}
 		else{
-			
+
 		}
-		
+
 		winner.SaveChanges();
 		loser.SaveChanges();
+
+		Map<String, Integer> winnerGamesPoints = winner.getStats().getGamesPoints();
+		int previousWinnerGamePoints = winnerGamesPoints.get(moishdGame.getGameType());
+		winnerGamesPoints.put(moishdGame.getGameType(), previousWinnerGamePoints + addedPoints[0]);
 		
+		Map<String, Integer> loserGamesPoints = loser.getStats().getGamesPoints();
+		int previousLoserGamePoints = loserGamesPoints.get(moishdGame.getGameType());
+		loserGamesPoints.put(moishdGame.getGameType(), previousLoserGamePoints + addedPoints[1]);
+
 		return addedPoints;
 	}
-	
+
 	private void updateRankAndTrophies(HashMap<String, String> payload, MoishdUser user) {
-		
+
 		updateRank(payload, user);
 
 		LoggerCommon.Get().LogInfo(this, "starting trophies calc ");
-		
+
 		List<TrophiesEnum> trophies = user.getTrophies();
-		
+
 		String tropiesAchieved = "";
 		int numOfTrophiesObtained = 0;
-		
+
 		int numOfWinsInARow = user.getStats().getGamesWonInARow();
 		LoggerCommon.Get().LogInfo(this, "numOfWinsInARow " + numOfWinsInARow );
 
@@ -258,10 +225,10 @@ public class SendGameResultServlet extends HttpServlet {
 			numOfTrophiesObtained++;
 			tropiesAchieved = tropiesAchieved + "#" + TrophiesEnum.TwentyInARow.toString();
 		}
-		
+
 		int numOfWins = user.getStats().getGamesWon();
 		LoggerCommon.Get().LogInfo(this, "gamesWon " + numOfWins );
-		
+
 		if  (numOfWins == 1 && !trophies.contains(TrophiesEnum.FirstTime)){
 			LoggerCommon.Get().LogInfo(this, "firstTime");
 
@@ -295,17 +262,17 @@ public class SendGameResultServlet extends HttpServlet {
 			tropiesAchieved = tropiesAchieved + "#" + TrophiesEnum.MegaMoisher.toString();
 		}
 		user.SaveChanges();
-		
+
 		payload.put("Trophies", tropiesAchieved);
 		payload.put("NumberOfTrophies", String.valueOf(numOfTrophiesObtained));
 
 	}
-	
+
 	private void updateRank(HashMap<String, String> payload, MoishdUser user){
-		
+
 		int userUpdatedPoints = user.getStats().getPoints();
 		LoggerCommon.Get().LogInfo(this, "currentPoints " + userUpdatedPoints);
-		
+
 		int currentRank = user.getStats().getRank();
 		if (100 > userUpdatedPoints && userUpdatedPoints >= 50 && currentRank == 0){
 			user.getStats().setRank(1);
@@ -328,6 +295,6 @@ public class SendGameResultServlet extends HttpServlet {
 			payload.put("Rank", "5");
 		}
 		user.SaveChanges();
-		
+
 	}
 }
