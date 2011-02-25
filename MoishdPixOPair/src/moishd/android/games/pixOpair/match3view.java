f50700908f401ce81dd0dc231f0e8d42e37651b2
hila.barzilai <hila.barzilai@d2c90892-111c-e7c7-7384-095ed294f940>
2011-02-25 15:09:08 +0000

    adding pix-o-pair

---
+++ MoishdPixOPair/src/moishd/android/games/pixOpair/match3view.java
@@ -0,0 +1,18 @@
+package moishd.android.games.pixOpair;
+
+import moishd.android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class match3view extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.match3);
+
+	}
+
+}
+
